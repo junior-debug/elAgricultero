bca7297379699a0cc97b18334e5b4873bf4d9fa2
FariaSta <davidfarias30@gmail.com>
2023-07-04 15:08:10 -0400

    header and menu burger

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,29 +1,66 @@
 "use client";
 
+import { useState } from "react";
+
 import Image from "next/image";
-import style from "./style.module.css";
+import "./style.css";
 
 import logoBlanco from "@/assets/brand/logo-blanco.png";
 import logoOriginal from "@/assets/brand/logo-original.png";
+import logoNice from "@/assets/brand/original-sinfondo.png";
+import x from "@/assets/icons/x.png";
 import burger from "@/assets/icons/menu.png";
 
 export default function Header() {
+  const [stateMenuBurger, setStateMenuBurger] = useState(false);
+
+  const toggleMenu = () => {
+    setStateMenuBurger( !stateMenuBurger )
+  }
+
   return (
-    <header className={style.header}>
-      <div className={style.containerLogo}>
-        <Image src={logoBlanco} className={style.logoBlanco} />
-        <Image src={logoOriginal} className={style.logoOriginal} />
+    <header className="header">
+      <div className="containerLogo">
+        <Image src={logoBlanco} className="logoBlanco" alt="El Agricultero" />
+        <Image
+          src={logoOriginal}
+          className="logoOriginal"
+          alt="El Agricultero"
+        />
       </div>
-        <div className={style.nav}>
-          <h3>Inicio</h3>
-          <h3>Huerto</h3>
-          <h3>Contacto</h3>
-          <h3>Documentación</h3>
-          <h3>Mentorías</h3>
+      <div className="nav">
+        <h3>Inicio</h3>
+        <h3>Huerto</h3>
+        <h3>Contacto</h3>
+        <h3>Documentación</h3>
+        <h3>Mentorías</h3>
+      </div>
+      <div className="responsiveNav" onClick={ toggleMenu }>
+        <Image src={burger} />
+
+        <div className={`menuBurger ${ stateMenuBurger ? 'isActive' : '' }`}>
+          <Image
+            className="x"
+            src={x}
+            alt="x"
+            onClick={ toggleMenu }
+          />
+          <div className="containerLogoMenu">
+            <Image
+              src={logoNice}
+              className="logoOriginalMenu"
+              alt="El Agricultero"
+            />
+          </div>
+          <ul className="ul">
+            <li>Inicio</li>
+            <li>Huerto</li>
+            <li>Contacto</li>
+            <li>Documentación</li>
+            <li>Mentorías</li>
+          </ul>
         </div>
-        <div className={style.responsiveNav}>
-          <Image src={burger} />
-        </div>
+      </div>
     </header>
   );
 }
