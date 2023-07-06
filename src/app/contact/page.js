13aec55acdf64cc82fc29ac63a6006d2a07465cd
FariaSta <davidfarias30@gmail.com>
2023-07-06 14:10:50 -0400

    contact router

---
+++ src/app/contact/page.js
@@ -0,0 +1,7 @@
+export default function contact() {
+  return (
+    <main >
+      <h1>oiqefjoiqwjfdoi</h1>
+    </main>
+  )
+}
--- src/app/contacto/page.js
+++
@@ -1,9 +0,0 @@
-import Header from "@/components/header";
-export function Contact() {
-  return (
-    <>
-      <h1>hola</h1>
-      <Header></Header>
-    </>
-  );
-}
