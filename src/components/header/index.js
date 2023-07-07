d30889d20c8cf5bbdc29b0bc98d4bdee38b7ec8c
FariaSta <davidfarias30@gmail.com>
2023-07-07 15:59:45 -0400

    menu's redirect

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,6 +1,7 @@
 "use client";
 
 import { useState } from "react";
+import { useRouter } from "next/navigation";
 
 import Image from "next/image";
 import "./style.css";
@@ -12,6 +13,7 @@ import x from "@/assets/icons/x.png";
 import burger from "@/assets/icons/menu.png";
 
 export default function Header() {
+  const router = useRouter();
   const [stateMenuBurger, setStateMenuBurger] = useState(false);
 
   const toggleMenu = () => {
@@ -19,43 +21,46 @@ export default function Header() {
   };
 
   return (
-    <header className="header">
-      <div className="containerLogo">
-        <Image src={logoBlanco} className="logoBlanco" alt="El Agricultero" />
-        <Image
-          src={logoOriginal}
-          className="logoOriginal"
-          alt="El Agricultero"
-        />
-      </div>
-      <div className="nav">
-        <h3>Inicio</h3>
-        <h3>Huerto</h3>
-        <h3>Contacto</h3>
-        <h3>Documentación</h3>
-        <h3>Mentorías</h3>
-      </div>
-      <div className="responsiveNav" onClick={toggleMenu}>
-        <Image src={burger} />
-
-        <div className={`menuBurger ${stateMenuBurger ? "isActive" : ""}`}>
-          <Image className="x" src={x} alt="x" onClick={toggleMenu} />
-          <div className="containerLogoMenu">
-            <Image
-              src={logoNice}
-              className="logoOriginalMenu"
-              alt="El Agricultero"
-            />
-          </div>
-          <ul className="ul">
-            <li>Inicio</li>
-            <li>Huerto</li>
-            <li>Contacto</li>
-            <li>Documentación</li>
-            <li>Mentorías</li>
-          </ul>
+    <div>
+      <header className="header">
+        <div className="containerLogo">
+          <Image src={logoBlanco} className="logoBlanco" alt="El Agricultero" />
+          <Image
+            src={logoOriginal}
+            className="logoOriginal"
+            alt="El Agricultero"
+          />
         </div>
-      </div>
-    </header>
+        <div className="nav">
+          <h3 onClick={() => router.push("/")}>Inicio</h3>
+          <h3>Huerto</h3>
+          <h3 onClick={() => router.push("/contact")}>Contacto</h3>
+          <h3>Documentación</h3>
+          <h3>Mentorías</h3>
+        </div>
+        <div className="responsiveNav" onClick={toggleMenu}>
+          <Image src={burger} />
+
+          <div className={`menuBurger ${stateMenuBurger ? "isActive" : ""}`}>
+            <Image className="x" src={x} alt="x" onClick={toggleMenu} />
+            <div className="containerLogoMenu">
+              <Image
+                src={logoNice}
+                className="logoOriginalMenu"
+                alt="El Agricultero"
+              />
+            </div>
+            <ul className="ul">
+              <li onClick={() => router.push("/")}>Inicio</li>
+              <li>Huerto</li>
+              <li onClick={() => router.push("/contact")}>Contacto</li>
+              <li>Documentación</li>
+              <li>Mentorías</li>
+            </ul>
+          </div>
+        </div>
+      </header>
+      <div className="space"></div>
+    </div>
   );
 }
