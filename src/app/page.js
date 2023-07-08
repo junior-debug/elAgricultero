85a8d438931e6f437196028be7dc81d148c38743
junior-debug <juniorellol@gmail.com>
2023-07-08 20:53:08 +0200

    carousel

--- src/app/page.js
+++ src/app/page.js
@@ -1,45 +1,12 @@
 "use client";
-import Swiper from "swiper";
-import "swiper/css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import React from "react";
+import { Button } from "react-bootstrap";
 
 export default function Home() {
-  const swiper = new Swiper(".swiper", {
-    // Optional parameters
-    direction: "horizontal",
-    loop: true,
-
-    // If we need pagination
-    pagination: {
-      el: ".swiper-pagination",
-    },
-
-    // // Navigation arrows
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
-
-    // // And if we need scrollbar
-    scrollbar: {
-      el: ".swiper-scrollbar",
-    },
-  });
   return (
     <main>
-      <div className="swiper">
-        <div className="swiper-wrapper">
-          <div className="swiper-slide">Slide 1</div>
-          <div className="swiper-slide">Slide 2</div>
-          <div className="swiper-slide">Slide 3</div>
-        </div>
-
-        <div className="swiper-pagination"></div>
-
-        <div className="swiper-button-prev"></div>
-        <div className="swiper-button-next"></div>
-
-        <div className="swiper-scrollbar"></div>
-      </div>
+      <Button variant="primary">Mi botón</Button>
     </main>
   );
 }
