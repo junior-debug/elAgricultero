2a726278a20de4857469330daa424f17714208a2
FariaSta <davidfarias30@gmail.com>
2023-07-21 13:10:56 -0400

    files and folders name changed

--- src/components/greenBut.js
+++
@@ -1,8 +0,0 @@
-import style from "@/components/footer/style.module.css";
-export function GreenBut(props) {
-  return (
-    <>
-      <button className={style.butSubscribe}>{props.text}</button>
-    </>
-  );
-}
---
+++ src/components/greenBut/index.js
@@ -0,0 +1,8 @@
+import "./style.css";
+export function GreenBut(props) {
+  return (
+    <>
+      <button className="butSubscribe">{props.text}</button>
+    </>
+  );
+}
