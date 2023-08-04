07a95891307dc4558f7ea1e48a4c3a9028917216
FariaSta <davidfarias30@gmail.com>
2023-08-03 21:16:19 -0400

    modal animated and zustand added for global state

--- src/components/cardProduct/index.js
+++ src/components/cardProduct/index.js
@@ -1,30 +1,30 @@
 "use client";
 
 import "./style.css";
-
 import { useState } from "react";
-
 import { GreenBut } from "@/components/greenBut";
+import { useMenuStore } from "@/components/modal"
 
 export default function Card(props) {
 
-    const [fullDescription, setFullDescription] = useState(false);
+  const toggleModal = useMenuStore(state => state.toggleModalOn)
 
-    const toggleFullDescription = () => {
-        setFullDescription(!fullDescription);
-        if (!fullDescription) {
-          console.log(fullDescription)
-          setTimeout(() => {
-            setFullDescription(false)
-          }, 6000); 
-        }
-    };
+  const [fullDescription, setFullDescription] = useState(false);
 
-    let propsStyleButton = {
-      width: window.innerWidth < 447 ? 80 + '%' : window.innerWidth < 347 ? 100 + '%' : 60 + '%', 
-      fontSize: window.innerWidth < 1000 ? 16 + "px" : window.innerWidth < 447 ? 9 + "px" : "",
-      height: window.innerWidth < 1000 ? 30 + "px" :  window.innerWidth < 447 ? 13 + "px" : ""
-    }
+  const toggleFullDescription = () => {
+      setFullDescription(true);
+      if (!fullDescription) {
+        setTimeout(() => {
+          setFullDescription(false)
+        }, 6000); 
+      }
+  };
+
+  let propsStyleButton = {
+    width: window.innerWidth < 447 ? 80 + '%' : window.innerWidth < 347 ? 100 + '%' : 60 + '%', 
+    fontSize: window.innerWidth < 1000 ? 16 + "px" : window.innerWidth < 447 ? 9 + "px" : "",
+    height: window.innerWidth < 1000 ? 30 + "px" :  window.innerWidth < 447 ? 13 + "px" : ""
+  }
 
   return (
     <div className={`c-container-card ${fullDescription ? "isActive" : ""} card-${props.index}`} style={{  background: props.image, backgroundSize: 'cover', backgroundPosition: '50% 10%'}} onClick={toggleFullDescription}>
@@ -32,7 +32,7 @@ export default function Card(props) {
             <h1 className="c-title">{props.title}</h1>
             <div className={`c-text ${fullDescription ? "isActive" : ""}`}>
               <p className="c-letters">{props.description}</p>
-              <div className="c-containerButton">
+              <div className="c-containerButton" onClick={toggleModal}>
                 <GreenBut text="Adquiere el PDF" propsStyle={propsStyleButton} littleLetters={true} />
               </div>
             </div>
--- src/components/modal/index.js
+++ src/components/modal/index.js
@@ -1,9 +1,28 @@
+'use client';
 import "./style.css";
+import { create } from 'zustand'
+
+export const useMenuStore = create((set) => ({
+  stateMenu: false,
+  toggleModalOn: () => {
+    set({stateMenu: true})
+  },
+  toggleModalOff: () => {
+    set({stateMenu: false})
+  },
+}))
+
+
+
 
 export default function Modal(props) {
+
+  const stateMenu = useMenuStore(state => state.stateMenu)
+  const toggleModal = useMenuStore(state => state.toggleModalOff)
+
   return (
-    <div>
-      <div className="m-modalBackground"></div>
+    <div className={`m-allModalComponent ${stateMenu ? "isActive" : ""}`}>
+      <div className="m-modalBackground" onClick={toggleModal}></div>
       <div className="m-modalContainer">{props.children}</div>
     </div>
   );
